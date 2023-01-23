edf7dc95f3eeace32722af9b33f81e379376c185
shurikperunov@mail.ru <shurikperunov@mail.ru>
2023-01-23 16:57:24 +0500

    Добавляет пакет cors

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import { errors } from 'celebrate';
 import { router } from './routes/index.js';
 import { errorHandler } from './middlewares/error-handler.js';
@@ -8,10 +9,10 @@ import { errorHandler } from './middlewares/error-handler.js';
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+app.use(cors());
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 app.use(bodyParser.json());
 app.use(router);
-
 app.use(errors());
 app.use(errorHandler);
 
