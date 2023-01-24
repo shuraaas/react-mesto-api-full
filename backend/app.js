a96d89fa93c9366070ffa854f8dd6ab5ccb00b75
shurikperunov@mail.ru <shurikperunov@mail.ru>
2023-01-24 22:38:53 +0500

    Меняет место подключения env

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,13 @@
-import cors from 'cors';
-import 'dotenv/config.js';
+import dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import { errors } from 'celebrate';
 import { router } from './routes/index.js';
 import { errorHandler } from './middlewares/error-handler.js';
 
+dotenv.config();
 const { PORT, DB_ADDRESS } = process.env;
 const app = express();
 
